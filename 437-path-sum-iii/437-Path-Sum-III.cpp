75009c7cf8d2836fc27b524ac6949d66c27d3cb3
Ujjwal Goyal <166270778+Ujjwal0207@users.noreply.github.com>
2026-07-18 02:29:14 +0530

    [437] [Time Beats: 100.00%] [Memory Beats: 31.21%] - LeetPush

--- 437-path-sum-iii/437-Path-Sum-III.cpp
+++ 437-path-sum-iii/437-Path-Sum-III.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    unordered_map<long long, int> mp;
     int ans = 0;
 
     void dfs(TreeNode* root, long long sum, int target) {
@@ -7,24 +8,20 @@ public:
 
         sum += root->val;
 
-        if (sum == target)
-            ans++;
+        if (mp.count(sum - target))
+            ans += mp[sum - target];
+
+        mp[sum]++;
 
         dfs(root->left, sum, target);
         dfs(root->right, sum, target);
-    }
 
-    void solve(TreeNode* root, int target) {
-        if (!root) return;
-
-        dfs(root, 0, target);
-
-        solve(root->left, target);
-        solve(root->right, target);
+        mp[sum]--;
     }
 
     int pathSum(TreeNode* root, int targetSum) {
-        solve(root, targetSum);
+        mp[0] = 1;
+        dfs(root, 0, targetSum);
         return ans;
     }
 };
